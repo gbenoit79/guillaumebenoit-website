0b4dab2aa0c10730ce668edad6435f1ecbc74a05
Guillaume BENOIT <gbenoit79@gmail.com>
2015-01-30 16:38:22 +0100

    Add fixtures for Post entity

---
+++ src/Gbs/BlogBundle/DataFixtures/ORM/LoadCategory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gbs\BlogBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Gbs\BlogBundle\Entity\Post;
+
+class LoadPost implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $post = new Post();
+            $post->setTitle("#{$i} post");
+            $post->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus egestas dolor.');
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
