c66dd79738f135d8a7ca289f879d6c2096c80dcd
Guillaume BENOIT <gbenoit79@gmail.com>
2015-02-09 16:45:44 +0100

    Use doctrine migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Gbs\BlogBundle\GbsBlogBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
